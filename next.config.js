30eba9b0f762ff96651c274667a802169f383b1b
gustavohoze <114294109+gustavohoze@users.noreply.github.com>
2025-06-09 22:23:02 +0700

    fix: build configuration

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
       },
     ],
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio2025' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio2025/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
   trailingSlash: true,
 }
 
